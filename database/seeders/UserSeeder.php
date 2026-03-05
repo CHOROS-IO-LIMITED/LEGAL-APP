6911867de704cac730acb8cd89df41b6d32c438a
DevEnigma69 <franco.cj03@gmail.com>
2026-03-05 19:05:41 +0800

    push

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,7 +13,7 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->create([
+        User::create([
             'name' => 'John Doe',
             'email' => 'admin@gmail.com',
             'password' => bcrypt('password'),
