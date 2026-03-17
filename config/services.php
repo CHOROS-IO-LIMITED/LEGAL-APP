5b188e62c17b4caf241d1ce1995b20cb21aa4def
mackieyyy <mackieyyy35@gmail.com>
2026-03-17 17:24:11 +0800

    update services

--- config/services.php
+++ config/services.php
@@ -41,14 +41,9 @@ return [
         'redirect' => env('GOOGLE_REDIRECT_URI'),
     ],
 
-    'complycube' => [
-        'key' => env('COMPLYCUBE_API_KEY'),
-        'url' => env('COMPLYCUBE_BASE_URL'),
-    ],
-
     'gemini' => [
         'api_key' => env('GEMINI_API_KEY'),
-        'model' => env('GEMINI_MODEL', 'gemini-2.5-flash'),
+        'model' => env('GEMINI_MODEL', 'gemini-3-flash'),
     ],
 
 ];
